3684576d2948e05a59ea18a83327bf6e79aded47
Yuri Cunha <54660107+garotogordo@users.noreply.github.com>
2021-12-01 09:20:07 -0300

    corrigindo o formulario para envio de email

--- js/init.js
+++ js/init.js
@@ -4,123 +4,130 @@
 /*
 -----------------------------------------------------------------------------------*/
 
- jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-/*----------------------------------------------------*/
-/* FitText Settings
------------------------------------------------------- */
+   /*----------------------------------------------------*/
+   /* FitText Settings
+   ------------------------------------------------------ */
 
-    setTimeout(function() {
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-	 }, 100);
-
-
-/*----------------------------------------------------*/
-/* Smooth Scrolling
------------------------------------------------------- */
-
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash,
-	    $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+   setTimeout(function () {
+      $('h1.responsive-headline').fitText(1, {
+         minFontSize: '40px',
+         maxFontSize: '90px'
+      });
+   }, 100);
 
 
-/*----------------------------------------------------*/
-/* Highlight the current section in the navigation bar
-------------------------------------------------------*/
+   /*----------------------------------------------------*/
+   /* Smooth Scrolling
+   ------------------------------------------------------ */
 
-	var sections = $("section");
-	var navigation_links = $("#nav-wrap a");
+   $('.smoothscroll').on('click', function (e) {
+      e.preventDefault();
 
-	sections.waypoint({
+      var target = this.hash,
+         $target = $(target);
 
-      handler: function(event, direction) {
-
-		   var active_section;
-
-			active_section = $(this);
-			if (direction === "up") active_section = active_section.prev();
-
-			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
-
-         navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
-
-		},
-		offset: '35%'
-
-	});
-
-
-/*----------------------------------------------------*/
-/*	Make sure that #header-background-image height is
-/* equal to the browser height.
------------------------------------------------------- */
-
-   $('header').css({ 'height': $(window).height() });
-   $(window).on('resize', function() {
-
-        $('header').css({ 'height': $(window).height() });
-        $('body').css({ 'width': $(window).width() })
+      $('html, body').stop().animate({
+         'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+         window.location.hash = target;
+      });
    });
 
 
-/*----------------------------------------------------*/
-/*	Fade In/Out Primary Navigation
-------------------------------------------------------*/
+   /*----------------------------------------------------*/
+   /* Highlight the current section in the navigation bar
+   ------------------------------------------------------*/
 
-   $(window).on('scroll', function() {
+   var sections = $("section");
+   var navigation_links = $("#nav-wrap a");
 
-		var h = $('header').height();
-		var y = $(window).scrollTop();
+   sections.waypoint({
+
+      handler: function (event, direction) {
+
+         var active_section;
+
+         active_section = $(this);
+         if (direction === "up") active_section = active_section.prev();
+
+         var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+         navigation_links.parent().removeClass("current");
+         active_link.parent().addClass("current");
+
+      },
+      offset: '35%'
+
+   });
+
+
+   /*----------------------------------------------------*/
+   /*	Make sure that #header-background-image height is
+   /* equal to the browser height.
+   ------------------------------------------------------ */
+
+   $('header').css({
+      'height': $(window).height()
+   });
+   $(window).on('resize', function () {
+
+      $('header').css({
+         'height': $(window).height()
+      });
+      $('body').css({
+         'width': $(window).width()
+      })
+   });
+
+
+   /*----------------------------------------------------*/
+   /*	Fade In/Out Primary Navigation
+   ------------------------------------------------------*/
+
+   $(window).on('scroll', function () {
+
+      var h = $('header').height();
+      var y = $(window).scrollTop();
       var nav = $('#nav-wrap');
 
-	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
-	      nav.fadeOut('fast');
-	   }
-      else {
-         if (y < h*.20) {
+      if ((y > h * .20) && (y < h) && ($(window).outerWidth() > 768)) {
+         nav.fadeOut('fast');
+      } else {
+         if (y < h * .20) {
             nav.removeClass('opaque').fadeIn('fast');
-         }
-         else {
+         } else {
             nav.addClass('opaque').fadeIn('fast');
          }
       }
 
-	});
+   });
 
 
-/*----------------------------------------------------*/
-/*	Modal Popup
-------------------------------------------------------*/
+   /*----------------------------------------------------*/
+   /*	Modal Popup
+   ------------------------------------------------------*/
 
-    $('.item-wrap a').magnificPopup({
+   $('.item-wrap a').magnificPopup({
 
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
+      type: 'inline',
+      fixedContentPos: false,
+      removalDelay: 200,
+      showCloseBtn: false,
+      mainClass: 'mfp-fade'
 
-    });
+   });
 
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
-    });
+   $(document).on('click', '.popup-modal-dismiss', function (e) {
+      e.preventDefault();
+      $.magnificPopup.close();
+   });
 
 
-/*----------------------------------------------------*/
-/*	Flexslider
-/*----------------------------------------------------*/
+   /*----------------------------------------------------*/
+   /*	Flexslider
+   /*----------------------------------------------------*/
    $('.flexslider').flexslider({
       namespace: "flex-",
       controlsContainer: ".flex-container",
@@ -133,56 +140,44 @@
       randomize: false,
    });
 
-/*----------------------------------------------------*/
-/*	contact form
-------------------------------------------------------*/
+   /*----------------------------------------------------*/
+   /*	contact form
+   ------------------------------------------------------*/
 
-   $('form#contactForm button.submit').click(function() {
+   $('form#contactForm button.submit').click(function () {
 
-      $('#image-loader').fadeIn();
 
-      var contactName = $('#contactForm #contactName').val();
-      var contactEmail = $('#contactForm #contactEmail').val();
-      var contactSubject = $('#contactForm #contactSubject').val();
-      var contactMessage = $('#contactForm #contactMessage').val();
 
-      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+      $(document).ready(function () {
+         $('.submit').click(function () {
+            $('#image-loader').fadeIn();
+            var contactName = $('#contactForm #contactName').val();
+            var contactEmail = $('#contactForm #contactEmail').val();
+            var contactSubject = $('#contactForm #contactSubject').val();
+            var contactMessage = $('#contactForm #contactMessage').val();
+            var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
                '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
-      $.ajax({
-
-	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
-
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
-
-	      }
-
+            $.ajax({
+               type: "POST",
+               url: "inc/sendEmail.php",
+               data: data,
+               success: function (msg) {
+                  if (msg == 'OK') {
+                     $('#image-loader').fadeOut();
+                     $('#message-warning').hide();
+                     $('#contactForm').fadeOut();
+                     $('#message-success').fadeIn();
+                  } else {
+                     $('#image-loader').fadeOut();
+                     $('#message-warning').html(msg);
+                     $('#message-warning').fadeIn();
+                  }
+               }
+            });
+            return false;
+         });
       });
-      return false;
    });
 
 
 });
-
-
-
-
-
-
-
-
